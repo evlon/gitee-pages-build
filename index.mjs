505c8074ca2e3cfc22bb5315185aa29f885d7011
niukunliang <niukunliang@cmict.chinamobile.com>
2021-11-25 15:53:40 +0800

    分割为2个函数

--- index.mjs
+++ index.mjs
@@ -3,17 +3,12 @@ import GiteePage from './giteePageBuild.mjs'
 import gist from './giteeGistSync.mjs'
 import dotenv from 'dotenv'
 
-export async function pagebuild_with_file_cookie () {
+
+
+export async function gitee_login_with_file_cookie () {
   if (!process.env.GITEE_USERNAME) {
     dotenv.config()
   }
-
-  console.log(process.env.GITEE_USERNAME,
-    process.env.GITEE_PASSWORD,
-    process.env.GITEE_REPO,
-    process.env.GITEE_BRANCH,
-    process.env.GITEE_DIRECTORY,
-    process.env.GITEE_HTTPS)
 
   const gitee = new GiteePage(process.env.GITEE_USERNAME,
     process.env.GITEE_PASSWORD,
@@ -22,6 +17,9 @@ export async function pagebuild_with_file_cookie () {
     process.env.GITEE_DIRECTORY,
     process.env.GITEE_HTTPS)
   try {
+      // add delay from env
+    GiteePage.delayFetch = process.env.CONFIG_DELAY_FETCH || 2000;
+
     // sync from gitee gist
     const syncResult = await gist.sync(process.env.GITEE_GIST_TOKEN, process.env.GITEE_GIST_ID, './.cookie.json')
     console.log('sync cookie to local ' + syncResult)
@@ -33,6 +31,34 @@ export async function pagebuild_with_file_cookie () {
 
     console.log('sync cookie to gitee gist  ' + saveResult)
     console.log('build ok')
+  
+    return true;
+  } catch (e) {
+    console.error(e)
+    return false;
+  }
+};
+
+export async function pagebuild_with_file_cookie () {
+  if (!process.env.GITEE_USERNAME) {
+    dotenv.config()
+  }
+
+  const gitee = new GiteePage(process.env.GITEE_USERNAME,
+    process.env.GITEE_PASSWORD,
+    process.env.GITEE_REPO,
+    process.env.GITEE_BRANCH,
+    process.env.GITEE_DIRECTORY,
+    process.env.GITEE_HTTPS)
+  try {
+      // add delay from env
+    GiteePage.delayFetch = process.env.CONFIG_DELAY_FETCH || 2000;
+
+    // sync from gitee gist
+    const syncResult = await gist.sync(process.env.GITEE_GIST_TOKEN, process.env.GITEE_GIST_ID, './.cookie.json')
+    console.log('sync cookie to local ' + syncResult)
+    await gitee.setCookieStoreFile('.cookie.json')
+
     // build page
     const buildResult = await gitee.pageBuild()
     console.log('pageBuild ' + buildResult)
@@ -49,17 +75,11 @@ export async function pagebuild_with_file_cookie () {
 };
 
 
-export async function pagebuild_with_obj_cookie () {
+export async function gitee_login_with_obj_cookie () {
   if (!process.env.GITEE_USERNAME) {
     dotenv.config()
   }
 
-  console.log(process.env.GITEE_USERNAME,
-    process.env.GITEE_PASSWORD,
-    process.env.GITEE_REPO,
-    process.env.GITEE_BRANCH,
-    process.env.GITEE_DIRECTORY,
-    process.env.GITEE_HTTPS)
 
   const gitee = new GiteePage(process.env.GITEE_USERNAME,
     process.env.GITEE_PASSWORD,
@@ -81,6 +101,40 @@ export async function pagebuild_with_obj_cookie () {
     // login gitee , when cookie is ok ,skip login api
     const loginResult = await gitee.login()
     console.log('login ' + syncResult)
+       // save cookie to gitee gist
+    jsonObject = await gitee.getCookieStoreJsonObject();
+    const saveResult = await gist.save_obj(process.env.GITEE_GIST_TOKEN, jsonObject, process.env.GITEE_GIST_ID,"json_cookie.mem")
+    console.log('sync cookie to gitee gist  ' + saveResult)
+    return true;
+  } catch (e) {
+    console.error(e)
+    return false;
+  }
+};
+
+export async function pagebuild_with_obj_cookie () {
+  if (!process.env.GITEE_USERNAME) {
+    dotenv.config()
+  }
+
+
+  const gitee = new GiteePage(process.env.GITEE_USERNAME,
+    process.env.GITEE_PASSWORD,
+    process.env.GITEE_REPO,
+    process.env.GITEE_BRANCH,
+    process.env.GITEE_DIRECTORY,
+    process.env.GITEE_HTTPS)
+  try {
+
+    // add delay from env
+    GiteePage.delayFetch = process.env.CONFIG_DELAY_FETCH || 2000;
+
+    let jsonObject = {};
+    // sync from gitee gist
+    const syncResult = await gist.sync_obj(process.env.GITEE_GIST_TOKEN, process.env.GITEE_GIST_ID, "json_cookie.mem", jsonObject)
+    console.log('sync cookie to local ' + syncResult)
+    await gitee.setCookieStoreJsonObject(jsonObject)
+    
     // build page
     const buildResult = await gitee.pageBuild()
     console.log('pageBuild ' + buildResult)
@@ -97,8 +151,9 @@ export async function pagebuild_with_obj_cookie () {
   }
 };
 
-
 export default {
+  gitee_login_with_file_cookie,
+  gitee_login_with_obj_cookie,
   pagebuild_with_file_cookie,
   pagebuild_with_obj_cookie
 }
--- test.mjs
+++ test.mjs
@@ -1,8 +1,16 @@
 import pageBuild from './index.mjs'
 
-pageBuild.pagebuild_with_obj_cookie();
- // or
-pageBuild.pagebuild_with_file_cookie();
+async function main_by_obj(){
+    await pageBuild.gitee_login_with_obj_cookie();
+
+    await pageBuild.pagebuild_with_obj_cookie();
+}
+
+
+main_by_obj()
+
+
+ 
 
 // gist.save(process.env.GITEE_GIST_TOKEN, "./.cookie.json", process.env.GITEE_GIST_ID);
 
